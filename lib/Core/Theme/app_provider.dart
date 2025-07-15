6e8f9771d2dd7c97f84a09a53ace3ead21212f78
kerolos <kokoessa951@gmail.com>
2025-07-15 21:09:10 +0300

    Last

--- lib/Core/Theme/app_provider.dart
+++ lib/Core/Theme/app_provider.dart
@@ -1,11 +1,16 @@
+import 'dart:convert';
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:projects_two/Core/api/api_service.dart';
+import 'package:projects_two/Core/api/api_state.dart';
 import '../../Features/user/Product/view/product_screen.dart';
 import '../../Features/Profile/view/profile_screen.dart';
 import '../../Features/user/Favorite/view/favorite_screen.dart';
 import '../../Features/user/Home/view/home_screen.dart';
 import '../../Features/user/cart/view/cart_screen.dart';
+import '../api/api_end_points.dart';
+import '../models/user_model.dart';
 import '../utils/app_constants.dart';
 
 import '../Services/preferences_manager.dart';
@@ -45,6 +50,29 @@ class AppProvider extends ChangeNotifier {
     this.filter = filter;
     log('this.filter: ${this.filter}');
     currentIndex = 1;
+    notifyListeners();
+  }
+
+  //? Get User Image
+  ApiState state = ApiState.initial;
+  ApiService apiService = ApiService();
+  String? image;
+  Future<void> getUserImage() async {
+    String userInfo = PreferencesManager.getString(AppConstants.userInfo)!;
+    UserModel localData = UserModel.fromJson(jsonDecode(userInfo));
+    state = ApiState.loading;
+    notifyListeners();
+    try {
+      final response = await apiService.get(
+        ApiEndPoints.getUserbyId(id: localData.id),
+      );
+      image = response.data["data"]["avatar"];
+      log('image:🤍 $image');
+      state = ApiState.success;
+    } catch (e) {
+      log('error: $e');
+      state = ApiState.error;
+    }
     notifyListeners();
   }
 }
