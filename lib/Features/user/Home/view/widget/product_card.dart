11cd44b9c87981424750d851eb04e77d2f0af597
kerolos <kokoessa951@gmail.com>
2025-07-21 17:00:28 +0300

    Refactor ProductCardHome widget layout and enhance price display logic

--- lib/Features/user/Home/view/widget/product_card.dart
+++ lib/Features/user/Home/view/widget/product_card.dart
@@ -17,101 +17,134 @@ class ProductCardHome extends StatelessWidget {
   });
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(2),
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.primaryContainer,
-        borderRadius: BorderRadius.circular(16),
-        boxShadow: [
-          BoxShadow(
-            offset: Offset(0, 2),
-            blurRadius: 2,
-            color: AppColors.black.withValues(alpha: .3),
-          ),
-        ],
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            flex: 3,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(16),
-              child: CachedNetworkImage(
-                imageUrl: "${ApiEndPoints.baseUrl}${productModel.imageCover}",
-                errorWidget: (context, url, error) =>
-                    const Icon(Icons.error, color: Colors.red),
-                width: double.infinity,
-                fit: BoxFit.cover,
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Container(
+          margin: const EdgeInsets.all(2),
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.primaryContainer,
+            borderRadius: BorderRadius.circular(16),
+            boxShadow: [
+              BoxShadow(
+                offset: Offset(0, 2),
+                blurRadius: 2,
+                color: AppColors.black.withValues(alpha: .3),
               ),
-            ),
+            ],
           ),
-          Expanded(
-            flex: 2,
-            child: Container(
-              width: .7.sw,
-              padding: const EdgeInsets.symmetric(horizontal: 7, vertical: 4),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  FittedBox(
-                    child: Text(
-                      productModel.title,
-                      style: Theme.of(context).textTheme.titleLarge,
-                    ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                flex: 3,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: CachedNetworkImage(
+                    imageUrl:
+                        "${ApiEndPoints.baseUrl}${productModel.imageCover}",
+                    errorWidget: (context, url, error) =>
+                        const Icon(Icons.error, color: Colors.red),
+                    width: double.infinity,
+                    fit: BoxFit.cover,
                   ),
-                  SizedBox(height: 2),
-                  Text(
-                    productModel.description,
-                    overflow: TextOverflow.ellipsis,
-                    style: Theme.of(
-                      context,
-                    ).textTheme.displayMedium?.copyWith(height: 1.4),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  width: .7.sw,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 7,
+                    vertical: 4,
                   ),
-                  Spacer(),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      productModel.priceAfterDiscount == 0
-                          ? const SizedBox()
-                          : Row(
-                              children: [
-                                Text(
-                                  productModel.price.toStringAsFixed(2),
-                                  style: TextStyle(
-                                    color: Color(0xff808488),
-                                    decoration: TextDecoration.lineThrough,
-                                    fontWeight: FontWeight.w600,
-                                  ),
-                                ),
-                                SizedBox(width: 10),
-                              ],
-                            ),
-                      Text(
-                        "${productModel.priceAfterDiscount.toString() == "0" ? productModel.price.toStringAsFixed(2) : productModel.priceAfterDiscount.toStringAsFixed(2)}\$",
-                        style: TextStyle(
-                          color: Color(0xffFA7189),
-                          fontWeight: FontWeight.w600,
-                          fontSize: 14.sp,
+                      FittedBox(
+                        child: Text(
+                          productModel.title,
+                          style: Theme.of(context).textTheme.titleLarge,
                         ),
                       ),
+                      SizedBox(height: 2),
+                      Text(
+                        productModel.description,
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(
+                          context,
+                        ).textTheme.displayMedium?.copyWith(height: 1.4),
+                      ),
+                      Spacer(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          productModel.priceAfterDiscount == 0
+                              ? const SizedBox()
+                              : Row(
+                                  children: [
+                                    Text(
+                                      productModel.price.toStringAsFixed(2),
+                                      style: TextStyle(
+                                        color: Color(0xff808488),
+                                        decoration: TextDecoration.lineThrough,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                    SizedBox(width: 10),
+                                  ],
+                                ),
+                          Text(
+                            "${productModel.priceAfterDiscount.toString() == "0" ? productModel.price.toStringAsFixed(2) : productModel.priceAfterDiscount.toStringAsFixed(2)}\$",
+                            style: TextStyle(
+                              color: Color(0xffFA7189),
+                              fontWeight: FontWeight.w600,
+                              fontSize: 14.sp,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 2),
+
+                      showRatings
+                          ? RatingBarWidget(
+                              initialRating:
+                                  double.tryParse(
+                                    productModel.ratingsAverage,
+                                  ) ??
+                                  0.0,
+                            )
+                          : SizedBox(),
                     ],
                   ),
-                  SizedBox(height: 2),
-
-                  showRatings
-                      ? RatingBarWidget(
-                          initialRating:
-                              double.tryParse(productModel.ratingsAverage) ??
-                              0.0,
-                        )
-                      : SizedBox(),
-                ],
+                ),
               ),
-            ),
+            ],
           ),
-        ],
-      ),
+        ),
+        productModel.priceAfterDiscount == 0
+            ? const SizedBox()
+            : Positioned(
+                top: 1,
+                right: 1,
+                child: Container(
+                  padding: const EdgeInsets.all(6),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.primaryContainer,
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(16),
+                      bottomLeft: Radius.circular(16),
+                    ),
+                  ),
+                  child: Text(
+                    "${(((productModel.price - productModel.priceAfterDiscount) / productModel.price) * 100).toStringAsFixed(1)} %",
+                    style: Theme.of(
+                      context,
+                    ).textTheme.titleMedium?.copyWith(color: AppColors.black),
+                  ),
+                ),
+              ),
+      ],
     );
   }
 }
