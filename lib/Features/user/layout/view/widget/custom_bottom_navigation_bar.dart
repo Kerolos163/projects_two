425fc47320cf69ef194607003177fba9e00fcca3
Helmy701 <ahmedhelmy1632000@gmail.com>
2025-07-21 06:14:37 +0300

    -change layout icons

--- lib/Features/user/layout/view/widget/custom_bottom_navigation_bar.dart
+++ lib/Features/user/layout/view/widget/custom_bottom_navigation_bar.dart
@@ -1,5 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../../Product/viewmodel/product_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -34,8 +35,9 @@ class CustomBottomNavigationBar extends StatelessWidget {
           label: AppStrings.home.tr(),
         ),
         BottomNavigationBarItem(
-          icon: SVGImage(
-            path: ImagePath.shoppingIcon,
+          icon: Icon(
+            Icons.style_outlined,
+            size: 30.r,
             color: appProvider.currentIndex == 1
                 ? AppColors.primary
                 : Theme.of(context).colorScheme.secondary,
@@ -61,12 +63,14 @@ class CustomBottomNavigationBar extends StatelessWidget {
           label: AppStrings.favorites.tr(),
         ),
         BottomNavigationBarItem(
-          icon: SVGImage(
-            path: ImagePath.settingsIcon,
+          icon: Icon(
+            Icons.person_outline,
+            size: 30.r,
             color: appProvider.currentIndex == 4
                 ? AppColors.primary
                 : Theme.of(context).colorScheme.secondary,
           ),
+
           label: AppStrings.profile2.tr(),
         ),
       ],
